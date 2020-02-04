16d7215b1f04921daadfe6243d710d0aa753a54f
Shital Patel <shital732@yahoo.com>
2020-02-03 22:19:58 -0500

    change css

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,15 @@
-var path = require('path');
-var express =require('express');
-var router = require('express').Router();
-var app =express();
-router.get("/",function(req, res) {
-  console.log("home page")
- res.sendFile(path.join(__dirname,"../public/index.html"));
-})
-router.get("/allnotes",function(req, res) {
-  console.log("all notes")
+var path = require("path");
+var express = require("express");
+var router = require("express").Router();
+var app = express();
+router.get("/", function(req, res) {
+  console.log("home page");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/allnotes", function(req, res) {
+  console.log("all notes");
   res.sendFile(path.join(__dirname, "../public/notes.html"));
-})
-
-
+});
 
 module.exports = router;
